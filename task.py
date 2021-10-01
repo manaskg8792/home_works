4e9723880018bfc9dc236db2e2e5c56fcdb3273f
Nazir <ainazirkg@gmail.com>
2021-10-01 15:08:50 +0600

    task

---
+++ task.py
@@ -0,0 +1,6 @@
+a = "Welcome to IT Bootcamp"
+my_word = input("Please, press 1 to add name \nif if want to apply press 2 \n")
+# if my_word == '1':
+#     my_list.append(input("Enter name: "))
+#
+#
